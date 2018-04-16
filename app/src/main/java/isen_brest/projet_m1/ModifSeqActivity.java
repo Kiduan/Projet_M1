4fc7335524b19f7a0346cbe3505eb3ac5555262e
bleunwenn <bleunwenn.allair@gmail.com>
2018-04-16 14:03:01 +0200

    Semaine 3 avec images

--- app/src/main/java/isen_brest/projet_m1/ModifSeqActivity.java
+++ app/src/main/java/isen_brest/projet_m1/ModifSeqActivity.java
@@ -1,14 +1,27 @@
 package isen_brest.projet_m1;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ModifSeqActivity extends AppCompatActivity {
 
     //déclaration du texte par défaut et du texte dans lequel on charge les données
     private TextView modif_text;
     private TextView modif_serial_description;
+    private Button take_picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,7 @@ public class ModifSeqActivity extends AppCompatActivity {
         //branchement des variables avec leur layout
         modif_text = (TextView) findViewById(R.id.modif_seq_default_text);
         modif_serial_description = (TextView) findViewById(R.id.modif_serial_description);
+        take_picture = (Button) findViewById(R.id.take_picture);
 
         //affichage des données importées depuis le clic précédent
         String newString;
@@ -33,4 +47,59 @@ public class ModifSeqActivity extends AppCompatActivity {
         }
         modif_serial_description.setText(newString);
     }
+
+  /*  String PhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  // prefix
+                ".jpg",         // suffix
+                storageDir      // directory
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        PhotoPath = image.getAbsolutePath();
+        return image;
+    }*/
+
+   //fonction prise de photo sur clic du bouton Photo
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void photo_click(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+/*    static final int REQUEST_TAKE_PHOTO = 1;
+
+    public void photo_click(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }*/
+
 }
+
+
